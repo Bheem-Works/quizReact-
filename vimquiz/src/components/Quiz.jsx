dbeac968dee8589dd0afe75cd81c80870e91e6ad
Bhim magar <shokokomichan098@gmail.com>
2025-05-03 23:17:09 +0545

    comments and the result push

--- vimquiz/src/components/Quiz.jsx
+++ vimquiz/src/components/Quiz.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import Result from './Result';
 
 //  storing the questions;
 const questionsBank = [
@@ -29,6 +30,7 @@ function Quiz() {
     const [userAnswer,setUserAnswer] = useState(intialAnswer); // why? when the user click the intial null become clicked value;
     const [currentQuestion,setCurrentQuestion] = useState(0); // why ? to pass the current question index 
     const selectedAnswer = userAnswer[currentQuestion]; // why ? to see if the user don't clikc it will not turn able to next buttons;
+    const [isQuizfinished,setIsQuizFinished] = useState(false); // why ? if the user is in the last page of the quiz and if they finish the quiz; 
 
     function handleSelectedOptions (option) { // why ? to see which options is selected 
         const newUserAnswer = [...userAnswer]; // why ? copying by using the thread operator to pass to the new users;
@@ -38,26 +40,39 @@ function Quiz() {
 
     
     function goToNext () {
+        // when the user completed the  question and  reach at the end question  4 === 4 then set it true; 
+        // why ? if it is true then it will pass it to the if statement
+        if( currentQuestion === questionsBank.length-1) { 
+            setIsQuizFinished(true); 
+        } else {
             setCurrentQuestion(currentQuestion + 1); 
+        }
         }
 
     function goToPrevious () {
         setCurrentQuestion(currentQuestion -1);
     }
-    return(
-        <div>
+
+    // if the is quizFinishes is true if they the user completed quiz then show the result Ui or then don't show;
+    if(isQuizfinished) {
+        return <Result />
+    } else {
+
+        return(
+            <div>
             <h2>Question {currentQuestion + 1 } </h2>  {/* why? when the user page load to the next  the current question will also be added from 1; */}
             <p>{questionsBank[currentQuestion].question}</p>
             {questionsBank[currentQuestion].options.map((option,index) => (
-                <button key={index} className={"option" +  (selectedAnswer === option ? " selected" : "")} onClick={() => handleSelectedOptions(option)}>{option}</button>
+                <button key= {index} className={"option" +  (selectedAnswer === option ? " selected" : "")} onClick={() => handleSelectedOptions(option)}>{option}</button>
             ))}
             <div className='nav-buttons'>
                 <button onClick={goToPrevious} disabled={currentQuestion === 0}>Previous</button>
-                <button onClick={goToNext} disabled={!selectedAnswer}>Next</button>
+                <button onClick={goToNext} disabled={!selectedAnswer}>{currentQuestion.length === 0 ? "Finish Quiz" : "Next"}</button>
             </div>
         </div>
     )
-   
+    
+}
 }
 
 export default Quiz
---
+++ vimquiz/src/components/Result.jsx
@@ -0,0 +1,11 @@
+function Result() {
+    return(
+        <div>
+            <h2>Quiz complited</h2>
+            <h2>You Score : 2/3</h2>
+            <button className="restart-button">RestartButton</button>
+        </div>
+    )
+}
+
+export default Result;
